c60adec0b6cbe3ecf01d607ec20be5c314a84827
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-25 00:02:34 +0300

    fix: add missing `API` endpoints

---
+++ RandomQuotesApp/Utils/Endpoints.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct Endpoints {
+    static let baseURL = ""
+    
+    static func getBaseURL() -> String {
+        return baseURL.isEmpty ? "http://localhost:8000/api" : baseURL
+    }
+    
+    struct Users {
+        static let signIn = "\(getBaseURL())/users/signin"
+        static let refreshTokens = "\(getBaseURL())/users/refresh-tokens"
+    }
+    
+    struct Quotes {
+        static let randomQuote = "\(getBaseURL())/quotes/random"
+    }
+}
