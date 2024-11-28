b4538dfdd96aa1eba4f831401e917fff837d5598
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-28 17:11:12 +0300

    refactor: `improve codebase` for `readability` (#24)

--- RandomQuotesApp/Utils/URLs.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct URLs {
-    static let privacyPolicy = "https://www.example.com/privacy"
-    static let termOfService = "https://www.example.com/terms"
-    static let email = "support@example.com"
-}
