df43986d1b78d869d4c685fa2db6fe77a87f8fe0
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-12-01 00:19:21 +0300

    feat: create `user interface` for `forgot password`

---
+++ RandomQuotesApp/Extensions/Auth/ForgotPasswordViewController+Extension.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension ForgotPasswordViewController {
+    
+    // MARK: - Keyboard Handling
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height / 3
+                self.authHeaderView.hideIconImageView()
+                self.navigationController?.navigationBar.isHidden = true
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+            self.authHeaderView.showIconImageView()
+            self.navigationController?.navigationBar.isHidden = false
+        }
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
