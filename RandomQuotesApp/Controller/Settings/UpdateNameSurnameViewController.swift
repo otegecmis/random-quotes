af864ca475ff982968f0e1430dd49f82bc4de44b
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-12-01 22:52:48 +0300

    feat: create `user interface` for updating `name` and `surname`

--- RandomQuotesApp/Controller/Settings/UpdateNameSurnameViewController.swift
+++ RandomQuotesApp/Controller/Settings/UpdateNameSurnameViewController.swift
@@ -2,6 +2,20 @@ import UIKit
 
 class UpdateNameSurnameViewController: UIViewController {
     
+    // MARK: - Properties
+    private let nameTextField = RQTextField(placeholder: "Name")
+    private let surnameTextField = RQTextField(placeholder: "Surname")
+    private let actionButton = RQButton(title: "Update", fontColor: .systemBackground, bgColor: .label)
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameTextField, surnameTextField, actionButton])
+        stackView.axis = .vertical
+        stackView.spacing = 13
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,11 +25,36 @@ class UpdateNameSurnameViewController: UIViewController {
     
     // MARK: - Helpers
     private func configureViewController() {
-        title = "Update Name Surname"
+        title = "Name Surname"
+        
+        actionButton.addTarget(self, action: #selector(actionTapped), for: .touchUpInside)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
     }
     
     private func configureUI() {
         view.backgroundColor = .systemBackground
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    // MARK: - Actions
+    @objc func actionTapped() {
+        guard let name = nameTextField.text, !name.isEmpty else {
+            presentAlertOnMainThread(title: "Empty Name", message: "Please enter your name.", buttonTitle: "Ok")
+            return
+        }
+        
+        guard let surname = surnameTextField.text, !surname.isEmpty else {
+            presentAlertOnMainThread(title: "Empty Surname", message: "Please enter your surname.", buttonTitle: "Ok")
+            return
+        }
+        
+        debugPrint("DEBUG: actionTapped()")
     }
 }
 
